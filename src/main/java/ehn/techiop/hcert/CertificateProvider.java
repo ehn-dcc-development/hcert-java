f3974ef186b9f8cb6df42993c7211897270e9933
jvi <jvi@trifork.com>
2021-04-17 22:53:13 +0200

    Corrected package name for autogenerated sources

--- src/main/java/ehn/techiop/hcert/CertificateProvider.java
+++ src/main/java/ehn/techiop/hcert/CertificateProvider.java
@@ -5,5 +5,5 @@ import COSE.CoseException;
 import java.security.PublicKey;
 
 public interface CertificateProvider {
-    PublicKey provideKey(String kid, String issuer) throws CoseException;
+    PublicKey provideKey(String kid, String issuer);
 }
